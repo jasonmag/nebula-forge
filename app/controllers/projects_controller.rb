e6f47150b649a7ae0ae009d82b55c260ab981b48
jasonmag <jasonmaglangit@gmail.com>
2024-03-12 01:47:50 +0800

    updated project forms

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -70,6 +70,6 @@ class ProjectsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def project_params
-      params.require(:project).permit(:title, :body, :tags, :image_source, :author_id, :published)
+      params.require(:project).permit(:title, :body, :tags, :image_source, :published)
     end
 end
