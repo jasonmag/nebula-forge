3552aa98ddc27dd58089590e6a306f888eea381e
jasonmag <jasonmaglangit@gmail.com>
2024-03-14 01:20:50 +0800

    added module for valid link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
 
+  def valid_link?(link)
+    uri = URI.parse(link)
+    uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+  rescue URI::InvalidURIError
+    false
+  end
+  
 end
