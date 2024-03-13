5a2665f6707fac7d45deff26b947ac5552282760
jasonmag <jasonmaglangit@gmail.com>
2024-03-14 01:19:54 +0800

    added projects collection on index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index,:about,:contact]
   before_action :get_blogs, only: [:index]
+  before_action :get_projects, only: [:index]
 
   def index
   end
@@ -15,5 +16,9 @@ class HomeController < ApplicationController
 
   def get_blogs
     @blogs = Blog.order(created_at: :desc).first(3)
+  end
+
+  def get_projects
+    @projects = Project.order(created_at: :desc).first(3)
   end
 end
