732cc9aaa44dc32ea9b1da65de8cb48aae50899b
jasonmag <jasonmaglangit@gmail.com>
2024-03-27 01:44:04 +0800

    updated subject

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,9 @@
 class ContactMailer < ApplicationMailer
-  default from: 'notification@jasonmag.com'
 
   def send_contact_email(name, email, message)
     @name = name
     @email = email
     @message = message
-    mail(to: 'jasonmaglangit@gmail.com', subject: 'Contact Us Submission')
+    mail(to: 'jasonmaglangit@gmail.com', subject: 'JasonMag - Contact Us Submission')
   end
 end 
