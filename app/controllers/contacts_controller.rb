f96bae0d1e5618b225b64a04620bce6e2c49592f
jasonmag <jasonmaglangit@gmail.com>
2024-03-28 00:26:53 +0800

    updated contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,8 +8,16 @@ class ContactsController < ApplicationController
     name = params[:name]
     email = params[:email]
     message = params[:message]
-
+    agree = params[:agree]
+  
+    if name.blank? || email.blank? || message.blank? || agree == '0'
+      @error_message = "All fields are required and agree must be true."
+      render :new
+      return
+    end
+  
     ContactMailer.send_contact_email(name, email, message).deliver_later
+  
     redirect_to root_path, notice: 'Message sent successfully!'
-  end
+  end  
 end
