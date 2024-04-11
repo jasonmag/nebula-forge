ea049f5f40d7460b15b1cf157e9a7471342fb1a5
jasonmag <jasonmaglangit@gmail.com>
2024-04-12 03:11:58 +0800

    added after sign_in route for admins

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -18,10 +18,17 @@ class Admins::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def after_sign_in_path_for(resource)
+    # Customize this method to redirect to the desired page after admin login
+    # For example, you can redirect to a specific URL or path
+    # projects_path
+    admins_dashboard_path
+  end 
 end
