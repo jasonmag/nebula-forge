060cd20ade010e567c1b682690646ade473065a4
jasonmag <jasonmaglangit@gmail.com>
2024-04-15 00:34:34 +0800

    added user methods for admin dashboards

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,6 +1,21 @@
 class Admins::UsersController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :get_users, only: [ :index ]
+  before_action :getuser, only: %i [ edit show ]
 
   def index
-    
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def get_users
+    @users = User.all
+  end
+
 end
